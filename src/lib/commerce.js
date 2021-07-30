72c32d24324527124f4a5cf159865575cf4680e8
Shubham <kumbharshubham206@gmail.com>
2021-07-30 20:28:30 +0530

    added add to git

---
+++ src/lib/commerce.js
@@ -0,0 +1,11 @@
+import Commerce from "@chec/commerce.js";
+
+// export const commerce = new Commerce(
+//   process.env.REACT_APP_CHEC_PUBLIC_KEY,
+//   true
+// );
+
+export const commerce = new Commerce(
+  "pk_310810edf28a9925696bde46f3a4de360d6b7eb4696aa",
+  true
+);
